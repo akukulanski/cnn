f1c6c0e01a7e3966a4b373dbeaebfb2b03c0cac9
akukulanski <akukulanski@gmail.com>
2020-03-23 00:27:03 -0300

    fix: required_bits

--- cnn/utils.py
+++ cnn/utils.py
@@ -3,7 +3,7 @@ from math import ceil, log2
 
 def required_bits(value):
     if value > 0: 
-        return ceil(log2(value + 1))
+        return ceil(1+log2(value + 1))
     else: 
         return ceil(log2(-value)+1) 
 
