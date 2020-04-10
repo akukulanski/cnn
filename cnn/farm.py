c430bbed1900aa83987791d616b2e12307465c57
akukulanski <akukulanski@gmail.com>
2020-04-09 23:59:04 -0300

    farm: small refactor

--- cnn/farm.py
+++ cnn/farm.py
@@ -2,6 +2,12 @@ from nmigen import *
 from cnn.dot_product import DotProduct
 from cores_nmigen.interfaces import AxiStream
 
+def _incr(signal, modulo):
+    if modulo == 2 ** len(signal):
+        return signal + 1
+    else:
+        return Mux(signal == modulo - 1, 0, signal + 1)
+
 class Farm(Elaboratable):
     def __init__(self, input_w, n_inputs, n_cores):
         self.input_w = input_w
@@ -28,20 +34,10 @@ class Farm(Elaboratable):
         current_core_sink = Signal(range(self.n_cores))
         current_core_source = Signal(range(self.n_cores))
 
-        # is this necessary?
-        # cores_input_array = Array([c.input for c in self.cores])
-        # cores_output_array = Array([c.output for c in self.cores])
-
-        # the farm input is the AXIS input interface of the current_core_sink
-        # comb += self.input.connect(cores_input_array[current_core_sink]) # comb += self.input.connect(self.cores[current_core_sink].input) # is this allowed?
-        # the farm output is the AXIs output interface of the current_core_source
-        # comb += self.output.connect(cores_output_array[current_core_source]) # comb += self.output.connect(self.cores[current_core_source].output) # is this allowed?
-        
         for i, core in enumerate(self.cores):
             m.submodules['core_' + str(i)] = core
             comb += [core.coeff[n].eq(self.coeff[n]) for n in range(self.n_inputs)] # same coefficients for everybody
             with m.If(current_core_sink == i):
-                # comb += self.input.connect(core.input)
                 comb += [core.input.valid.eq(self.input.valid),
                          core.input.data.eq(self.input.data),
                          self.input.ready.eq(core.input.ready),
@@ -51,7 +47,6 @@ class Farm(Elaboratable):
                          core.input.data.eq(0),
                         ]
             with m.If(current_core_source == i):
-                # comb += self.output.connect(core.output)
                 comb += [self.output.valid.eq(core.output.valid),
                          self.output.data.eq(core.output.data),
                          core.output.ready.eq(self.output.ready),
@@ -59,17 +54,10 @@ class Farm(Elaboratable):
             with m.Else():
                 comb += [core.output.ready.eq(0),
                         ]
-        
         with m.If(self.input.accepted()):
-            with m.If(current_core_sink < self.n_cores - 1):
-                sync += current_core_sink.eq(current_core_sink + 1)
-            with m.Else():
-                sync += current_core_sink.eq(0)
+            sync += current_core_sink.eq(_incr(current_core_sink, self.n_cores))
 
         with m.If(self.output.accepted()):
-            with m.If(current_core_source < self.n_cores - 1):
-                sync += current_core_source.eq(current_core_source + 1)
-            with m.Else():
-                sync += current_core_source.eq(0)
+            sync += current_core_source.eq(_incr(current_core_source, self.n_cores))
 
         return m
