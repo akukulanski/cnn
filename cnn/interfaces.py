ab15b198c76c59921a09e75a724ec576a1dab2f4
akukulanski <akukulanski@gmail.com>
2020-04-12 20:57:05 -0300

    moved auxiliary class inside the method that uses it

--- cnn/interfaces.py
+++ cnn/interfaces.py
@@ -2,18 +2,6 @@ from cores_nmigen.interfaces import MetaStream
 import cnn.matrix as mat
 
 
-class MatrixPort():
-
-    def __init__(self, interface):
-        self.interface = interface
-
-    def __getitem__(self, tup):
-        if not hasattr(tup, '__iter__'):
-            tup = (tup,)
-        assert len(tup) == len(self.interface.shape), f'{len(tup)} == {len(self.interface.shape)}'
-        return getattr(self.interface, self.interface.get_signal_name(tup))
-
-
 class AxiStreamMatrix(MetaStream):
 
     def __init__(self, width, shape, direction=None, name=None, fields=None):
@@ -27,11 +15,6 @@ class AxiStreamMatrix(MetaStream):
 
     def get_signal_name(self, indexes):
         return 'TDATA_' + '_'.join([str(i) for i in indexes])
-
-    @property
-    def matrix(self):
-        return MatrixPort(self)
-    
 
     @property
     def dimensions(self):
@@ -53,3 +36,13 @@ class AxiStreamMatrix(MetaStream):
         assert isinstance(other, AxiStreamMatrix)
         return [data_o.eq(data_i) for data_o, data_i in zip(self.data_ports, other.data_ports)]
 
+    @property
+    def matrix(self):
+        interface = self
+        class MatrixPort():
+            def __getitem__(self_mp, tup):
+                if not hasattr(tup, '__iter__'):
+                    tup = (tup,)
+                assert len(tup) == len(interface.shape), f'{len(tup)} == {len(interface.shape)}'
+                return getattr(interface, interface.get_signal_name(tup))
+        return MatrixPort()
