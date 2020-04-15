f6fec1d340d385be4c0d486c8ab3d5368287bb24
akukulanski <akukulanski@gmail.com>
2020-04-15 16:41:37 -0300

    matrix port in matrix_feeder

--- cnn/matrix_feeder.py
+++ cnn/matrix_feeder.py
@@ -2,6 +2,7 @@ from nmigen import *
 from cores_nmigen.interfaces import AxiStream
 from cores_nmigen.operations import _incr, _and, _or
 from cnn.row_fifos import RowFifos
+from cnn.interfaces import AxiStreamMatrix
 
 class MatrixFeeder(Elaboratable):
     """ N fifos that work synchronized to provide NxN matrixes
@@ -10,28 +11,42 @@ class MatrixFeeder(Elaboratable):
     signal only when the current NxN matrix corresponds to
     a valid NxN submatrix of the image.
     """
-    def __init__(self, input_w, row_length, N, endianness=-1):
-        self.input_w = input_w
+    def __init__(self, input_w, row_length, N, invert=False):
         self.row_length = row_length
-        self.N = N
-        self.endianness = endianness
-        self.output_w = input_w * N**2
-        self.input = AxiStream(width=self.input_w, direction='sink', name='input')
-        self.output = AxiStream(width=self.output_w, direction='source', name='output')
+        self.invert = invert
+        self.input = AxiStream(width=input_w, direction='sink', name='input')
+        self.output = AxiStreamMatrix(width=input_w, shape=(N,N), direction='source', name='output')
 
     def get_ports(self):
         ports = [self.input[f] for f in self.input.fields]
         ports += [self.output[f] for f in self.output.fields]
         return ports
 
+    @property
+    def input_w(self):
+        return self.input.width
+
+    @property
+    def output_w(self):
+        return self.input_w
+
+    @property
+    def shape(self):
+        return self.output.shape
+
+    @property
+    def N(self):
+        return self.output.shape[0]
+    
+
     def elaborate(self, platform):
         m = Module()
         sync = m.d.sync
         comb = m.d.comb
 
-        m.submodules.row_fifos = row_fifos = RowFifos(self.input_w, self.row_length, self.N, self.endianness)
+        m.submodules.row_fifos = row_fifos = RowFifos(self.input_w, self.row_length, self.N, self.invert)
+        m.submodules.submatrix_regs = submatrix = SubmatrixRegisters(self.input_w, self.N, self.invert)
 
-        registers = Array([Signal(self.input_w * self.N) for _ in range(self.N)])
         current_column = Signal(range(self.row_length))
 
         comb += [row_fifos.input.valid.eq(self.input.valid),
@@ -39,25 +54,84 @@ class MatrixFeeder(Elaboratable):
                  self.input.ready.eq(row_fifos.input.ready),
                 ]
 
-        with m.If(row_fifos.output.accepted()):
+        comb += [submatrix.input.valid.eq(row_fifos.output.valid),
+                 submatrix.input.connect_data_ports(row_fifos.output),
+                 row_fifos.output.ready.eq(submatrix.input.ready),
+                ]
+
+        comb += [self.output.connect_data_ports(submatrix.output),
+                ]
+
+        with m.If(submatrix.output.accepted()):
             sync += current_column.eq(_incr(current_column, self.row_length))
-            with m.If(current_column < self.N - 1):
-                sync += self.output.valid.eq(0)
-            with m.Else():
-                sync += self.output.valid.eq(1)
-            sync += registers[0].eq(row_fifos.output.data)
-            for n in range(1, self.N):
-                sync += registers[n].eq(registers[n-1])
+
+        # logic to dismiss data when the output matrix is not
+        # a valid submatrix of the input.
+        with m.If(current_column < self.N - 1):
+            comb += [self.output.valid.eq(0),
+                     submatrix.output.ready.eq(1),
+                    ]
+        with m.Else():
+            comb += [self.output.valid.eq(submatrix.output.valid),
+                     submatrix.output.ready.eq(self.output.ready),
+                    ]
+
+        return m
+
+
+class SubmatrixRegisters(Elaboratable):
+
+    def __init__(self, input_w, N, invert=False):
+        self.invert = invert
+        self.input = AxiStreamMatrix(width=input_w, shape=(N,), direction='sink', name='input')
+        self.output = AxiStreamMatrix(width=input_w, shape=(N,N), direction='source', name='output')
+
+    def get_ports(self):
+        ports = [self.input[f] for f in self.input.fields]
+        ports += [self.output[f] for f in self.output.fields]
+        return ports
+
+    @property
+    def input_w(self):
+        return self.input.width
+
+    @property
+    def output_w(self):
+        return self.output.width
+
+    @property
+    def shape_i(self):
+        return self.input.shape
+
+    @property
+    def shape_o(self):
+        return self.output.shape
+
+    @property
+    def N(self):
+        return self.output.shape[0]
+
+    def elaborate(self, platform):
+        m = Module()
+        sync = m.d.sync
+        comb = m.d.comb
+
+        if self.invert:
+            _col = lambda col: col
+        else:
+            _col = lambda col: self.N - 1 - col
+
+        with m.If(self.input.accepted()):
+            for row in range(self.N): # row iteration
+                sync += self.output.matrix[row, _col(0)].eq(self.input.matrix[row]) # append column from input
+                for col in range(1, self.N): # shift to the right the other columns
+                    sync += self.output.matrix[row, _col(col)].eq(self.output.matrix[row, _col(col-1)])
+
+        with m.If(self.input.accepted()):
+            sync += self.output.valid.eq(1)
         with m.Elif(self.output.accepted()):
             sync += self.output.valid.eq(0)
 
-        comb += row_fifos.output.ready.eq(self.output.accepted() | ~self.output.valid)
-
-        for row in range(self.N):
-            start_idx = self.input_w * self.N * row
-            stop_idx = self.input_w * self.N * (row + 1)
-            reg_start_idx = self.input_w * row
-            reg_stop_idx = self.input_w * (row + 1)
-            comb += self.output.data[start_idx:stop_idx].eq(Cat(*[registers[n][reg_start_idx:reg_stop_idx] for n in range(self.N)[::self.endianness]]))
+        comb += self.input.ready.eq(self.output.accepted() | ~self.output.valid)
 
         return m
--- cnn/tests/test_matrix_feeder.py
+++ cnn/tests/test_matrix_feeder.py
@@ -1,11 +1,8 @@
 from nmigen_cocotb import run
 from cnn.matrix_feeder import MatrixFeeder
-from cnn.tests.utils import twos_comp_from_int, int_from_twos_comp, slice_signal
 from cores_nmigen.test.interfaces import AxiStreamDriver
-from cores_nmigen.test.utils import pack, unpack
+from cnn.tests.interfaces import AxiStreamMatrixDriver
 import pytest
-import random
-from math import ceil, log2
 import numpy as np
 import os
 
@@ -14,71 +11,64 @@ try:
     from cocotb.triggers import RisingEdge
     from cocotb.clock import Clock
     from cocotb.regression import TestFactory as TF
-    from .interfaces import *
 except:
     pass
 
 CLK_PERIOD_BASE = 100
-random.seed()
-
-class MatrixFeederTest():
-    def __init__(self, dut, row_length):
-        self.dut = dut
-        self.row_length = row_length
-        self.input_w = len(self.dut.input__TDATA)
-        self.output_w = len(self.dut.output__TDATA)
-
-    @property
-    def N(self):
-        return int((self.output_w / self.input_w)**0.5)
-
-    def get_output(self, idx):
-        inputs = list(unpack([self.dut.output__TDATA.value.integer], self.N, self.input_w))
-        return inputs[idx]
-
-    @cocotb.coroutine
-    def init_test(self):
-        self.dut.input__TVALID <= 0
-        self.dut.input__TLAST <= 0
-        self.dut.input__TDATA <= 0
-        self.dut.output__TREADY <= 0
-        self.dut.rst <= 1
-        cocotb.fork(Clock(self.dut.clk, 10, 'ns').start())
-        yield RisingEdge(self.dut.clk)
-        self.dut.rst <= 0
-        yield RisingEdge(self.dut.clk)
-
-    def generate_random_image(self, height):
-        limits = (-2**(self.input_w - 1), 2**(self.input_w - 1) - 1)
-        return [random.randint(limits[0], limits[1]) for _ in range(self.row_length * height)]
-
-    def generate_incremental_image(self, height):
-        limits = (-2**(self.input_w - 1), 2**(self.input_w - 1) - 1)
-        return [int(i % limits[1]) for i in range(self.row_length * height)]
-
-    def check_data(self, buff_in, buff_out):
-        width, height = self.row_length, int(len(buff_in) / self.row_length)
-        input_image = np.reshape(buff_in, (height, width))
-        for i, output in enumerate(buff_out):
-            matrix_output = np.reshape(list(unpack([output], self.N ** 2, self.input_w)), (self.N, self.N))
-            idx_x = i % (width + 1 - self.N)
-            idx_y = int(i / (width + 1 - self.N))
-            expected_submatrix = input_image[idx_y:idx_y+self.N, idx_x:idx_x+self.N]
-            assert (matrix_output == expected_submatrix).all(), f'output[{i}]: (x,y)={(idx_x,idx_y)}\n{matrix_output}\n!=\n{expected_submatrix}'
 
 
 @cocotb.coroutine
-def check_data(dut, width, height, burps_in, burps_out, dummy=0):
-    test_size = 20
+def init_test(dut):
+    dut.rst <= 1
+    cocotb.fork(Clock(dut.clk, 10, 'ns').start())
+    yield RisingEdge(dut.clk)
+    dut.rst <= 0
+    yield RisingEdge(dut.clk)
 
-    test = MatrixFeederTest(dut, width)
-    yield test.init_test()
+def get_pixel(buffer, x, y, row_length):
+    return buffer[row_length * y + x]
+
+def incremental_matrix(shape, size, max_value):
+    data = []
+    count = 0
+    for i in range(size):
+        matrix = mat.create_empty_matrix(shape)
+        for idx in mat.matrix_indexes(shape):
+            mat.set_matrix_element(matrix, idx, count)
+            count = (count + 1) % max_value
+        data.append(matrix)
+    return data
+
+def check_monitors_data(buff_in, buff_out, width, height, N, invert=False):
+    input_image = np.reshape(buff_in, (height, width))
+    for i, output_matrix in enumerate(buff_out):
+        assert np.shape(output_matrix) == (N, N), f'{np.shape(output_matrix)} != {(N, N)})'
+        idx_x = i % (width + 1 - N)
+        idx_y = int(i / (width + 1 - N))
+        expected_submatrix = input_image[idx_y:idx_y+N, idx_x:idx_x+N]
+        if invert:
+            expected_submatrix = expected_submatrix[::-1, ::-1]
+        assert (output_matrix == expected_submatrix).all(), f'output[{i}]: (x,y)={(idx_x,idx_y)}\n{output_matrix}\n!=\n{expected_submatrix}'
+
+
+@cocotb.coroutine
+def check_data(dut, N, width, height, invert=False, burps_in=False, burps_out=False, dummy=0):
+
+    yield init_test(dut)
 
     m_axis = AxiStreamDriver(dut, name='input_', clock=dut.clk)
-    s_axis = AxiStreamDriver(dut, name='output_', clock=dut.clk)
-    
-    wr_data = test.generate_incremental_image(height)
-    expected_output_length = (width + 1 - test.N) * (height + 1 - test.N)
+    s_axis = AxiStreamMatrixDriver(dut, name='output_', clock=dut.clk, shape=(N,N))
+    input_w = len(dut.input__TDATA)
+    output_w = len(s_axis.get_element(s_axis.first_idx))
+    m_axis.bus.TVALID <= 0
+    m_axis.bus.TLAST <= 0
+    m_axis.bus.TDATA <= 0
+    s_axis.init_source()
+    yield RisingEdge(dut.clk)
+
+    image_size = width * height
+    wr_data = wr_b = [int(x % (2**input_w-1)) for x in range(image_size)]
+    expected_output_length = (width + 1 - N) * (height + 1 - N)
 
     cocotb.fork(m_axis.monitor())
     cocotb.fork(s_axis.monitor())
@@ -91,43 +81,43 @@ def check_data(dut, width, height, burps_in, burps_out, dummy=0):
 
     dut._log.info(f'Buffer in length: {len(m_axis.buffer)}.')
     dut._log.info(f'Buffer out length: {len(s_axis.buffer)}.')
+    assert len(m_axis.buffer) == len(wr_data), f'{len(m_axis.buffer)} != {len(wr_data)}'
     assert len(s_axis.buffer) == expected_output_length, f'{len(s_axis.buffer)} != {expected_output_length}'
-
-    # print debug data
-    dut._log.debug(f'\ninput image:\n{np.reshape(wr_data, (width, height))}')
-    for i, d in enumerate(s_axis.buffer):
-        tmp = list(unpack([d], test.N ** 2, test.input_w))
-        dut._log.debug(f'\noutput matrix #{i}: {tmp}\n{np.reshape(tmp, (test.N, test.N))}')
-
-    test.check_data(m_axis.buffer, s_axis.buffer)
+    
+    check_monitors_data(buff_in=m_axis.buffer, buff_out=s_axis.buffer,
+                        width=width, height=height, N=N, invert=invert)
 
 
 try:
     running_cocotb = True
-    width = int(os.environ['coco_param_row_length'], 10)
     N = int(os.environ['coco_param_N'], 10)
+    width = int(os.environ['coco_param_row_length'], 10)
+    invert = int(os.environ['coco_param_invert'], 10)
 except KeyError as e:
     running_cocotb = False
 
 if running_cocotb:
     tf_test_data = TF(check_data)
+    tf_test_data.add_option('N', [N])
     tf_test_data.add_option('width', [width])
     tf_test_data.add_option('height', [5])
-    tf_test_data.add_option('burps_in', [False, True]) # TO DO: FIX. Test NO PASA con burps_in==True.
-    tf_test_data.add_option('burps_out', [False, True])
+    tf_test_data.add_option('invert', [invert])
+    # tf_test_data.add_option('burps_in', [False, True])
+    # tf_test_data.add_option('burps_out', [False, True])
     tf_test_data.generate_tests()
 
 
 @pytest.mark.timeout(10)
-@pytest.mark.parametrize("input_w, row_length, N, endianness", [(8, 5, 3, -1),
-                                                                # (8, 5, 3, +1),
-                                                               ])
-def test_matrix_feeder(input_w, row_length, N, endianness):
-    os.environ['coco_param_row_length'] = str(row_length)
+@pytest.mark.parametrize("input_w, row_length, N, invert", [(8, 5, 3, False),
+                                                            (8, 5, 3, True),
+                                                           ])
+def test_matrix_feeder(input_w, row_length, N, invert):
     os.environ['coco_param_N'] = str(N)
+    os.environ['coco_param_row_length'] = str(row_length)
+    os.environ['coco_param_invert'] = str(int(invert))
     core = MatrixFeeder(input_w=input_w,
                         row_length=row_length,
                         N=N,
-                        endianness=endianness)
+                        invert=invert)
     ports = core.get_ports()
-    run(core, 'cnn.tests.test_matrix_feeder', ports=ports, vcd_file=f'./test_matrix_feeder_i{input_w}_rowlength{row_length}_N{N}.vcd')
+    run(core, 'cnn.tests.test_matrix_feeder', ports=ports, vcd_file=f'./test_matrix_feeder_i{input_w}_rowlength{row_length}_N{N}_invert{invert}.vcd')
