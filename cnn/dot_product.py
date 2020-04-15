fe6bdc7a99b6ff5979489c9d950eb1db1e57d4f2
akukulanski <akukulanski@gmail.com>
2020-04-15 16:40:35 -0300

    matrix port in dot_product

--- cnn/dot_product.py
+++ cnn/dot_product.py
@@ -1,23 +1,48 @@
 from nmigen import *
 from cnn.mac import MAC
 from cnn.utils import required_bits
+from cnn.interfaces import AxiStreamMatrix
+import cnn.matrix as mat
 from cores_nmigen.interfaces import AxiStream
 
 class DotProduct(Elaboratable):
-    def __init__(self, input_w, n_inputs):
-        self.input_w = input_w
-        self.n_inputs = n_inputs
-        self.output_w = self._calculate_output_width()
-        self.input = AxiStream(width=self.input_w*self.n_inputs, direction='sink', name='input')
-        self.coeff = [Signal(self.input_w, name='coeff_'+str(i)) for i in range(self.n_inputs)] # To do: unify both input interfaces.
+    #
+    # WARNING:
+    # The dataflow is controlled ONLY by the input_a AXIS interface.
+    # The input_b AXIS interface is DUMMY, and should always have valid values in the input.
+    # The ready of the input_b interface will be attached to input_a.accepted(), and a valid=1
+    # will be assumed.
+    #
+    # Why?
+    # I want to avoid a combinational path between the valid of input_b and the ready of input_a.
+    #
+    def __init__(self, input_w, shape):
+        self.input_a = AxiStreamMatrix(width=input_w, shape=shape, direction='sink', name='input_a')
+        self.input_b = AxiStreamMatrix(width=input_w, shape=shape, direction='sink', name='input_b')
         self.output = AxiStream(self.output_w, direction='source', name='output')
 
     def get_ports(self):
         ports = []
-        ports += [coeff for coeff in self.coeff]
-        ports += [self.input[f] for f in self.input.fields]
+        ports += [self.input_a[f] for f in self.input_a.fields]
+        ports += [self.input_b[f] for f in self.input_b.fields]
         ports += [self.output[f] for f in self.output.fields]
         return ports
+    
+    @property
+    def input_w(self):
+        return self.input_a.width
+    
+    @property
+    def n_inputs(self):
+        return self.input_a.n_elements
+
+    @property
+    def shape(self):
+        return self.input_a.shape
+
+    @property
+    def output_w(self):
+        return self._calculate_output_width()
 
     def _calculate_output_width(self):
         worst_value = -2**(self.input_w - 1)
@@ -30,28 +55,29 @@ class DotProduct(Elaboratable):
         sync = m.d.sync
         comb = m.d.comb
 
-        tmp_input = Signal(self.input_w * self.n_inputs)
-        tmp_coeff = Signal(self.input_w * self.n_inputs)
+        tmp_input_a = Signal(self.input_w * self.n_inputs)
+        tmp_input_b = Signal(self.input_w * self.n_inputs)
         counter = Signal(range(self.n_inputs))
-
+        
         m.submodules['mac'] = mac = MAC(input_w=self.input_w, output_w=self.output_w)
-
-        comb += [mac.input_a.eq(tmp_input[0:self.input_w]),
-                 mac.input_b.eq(tmp_coeff[0:self.input_w]),]
-
+        comb += [mac.input_a.eq(tmp_input_a[0:self.input_w]),
+                 mac.input_b.eq(tmp_input_b[0:self.input_w]),]
+        
+        # DUMMY input_b interface
+        comb += [self.input_b.ready.eq(self.input_a.accepted())]
     
         with m.FSM() as fsm:
             
             with m.State("IDLE"):
             
-                comb += [self.input.ready.eq(self.output.accepted() | ~self.output.valid),
+                comb += [self.input_a.ready.eq(self.output.accepted() | ~self.output.valid),
                          mac.clr.eq(1),
                          mac.clken.eq(0),]
             
-                with m.If(self.input.accepted()):
+                with m.If(self.input_a.accepted()):
                     m.next = "BUSY"
-                    sync += [tmp_input.eq(self.input.data),
-                             tmp_coeff.eq(Cat(*self.coeff)),
+                    sync += [tmp_input_a.eq(Cat(*self.input_a.flatten_matrix)), #self.input_a.data),
+                             tmp_input_b.eq(Cat(*self.input_b.flatten_matrix)), #Cat(*self.input_b)),
                              counter.eq(0),]
             
                 with m.If(self.output.accepted()):
@@ -59,12 +85,12 @@ class DotProduct(Elaboratable):
             
             with m.State("BUSY"):
             
-                comb += [self.input.ready.eq(0),
+                comb += [self.input_a.ready.eq(0),
                          mac.clr.eq(0),
                          mac.clken.eq(1),]
             
-                sync += [tmp_coeff.eq(tmp_coeff >> self.input_w),
-                         tmp_input.eq(tmp_input >> self.input_w),]
+                sync += [tmp_input_b.eq(tmp_input_b >> self.input_w),
+                         tmp_input_a.eq(tmp_input_a >> self.input_w),]
             
                 with m.If(mac.valid_o):
                     sync += counter.eq(counter + 1)
--- cnn/tests/test_dot_product.py
+++ cnn/tests/test_dot_product.py
@@ -1,12 +1,13 @@
 from nmigen_cocotb import run
 from cnn.dot_product import DotProduct
-from cnn.tests.utils import twos_comp_from_int, int_from_twos_comp
+from cnn.tests.utils import int_from_twos_comp
+import cnn.matrix as mat
+from cnn.tests.interfaces import AxiStreamMatrixDriver
 from cores_nmigen.test.interfaces import AxiStreamDriver
-from cores_nmigen.test.utils import pack, unpack
 import pytest
 import random
-from math import ceil, log2
 import numpy as np
+import os
 
 try:
     import cocotb
@@ -20,126 +21,92 @@ except:
 CLK_PERIOD_BASE = 100
 random.seed()
 
-class DotProductTest():
-    def __init__(self, dut):
-        self.dut = dut
-        self.input_w = int(len(self.dut.input__TDATA) / self.n_inputs)
-        self.output_w = len(self.dut.output__TDATA)
-        self.buff_in = []
-        self.buff_coeffs = []
-        self.buff_out = []
-
-    @property
-    def n_inputs(self):
-        i = 0
-        while hasattr(self.dut, 'coeff_' + str(i)):
-            i += 1
-        return i
-
-    def get_input(self, idx):
-        inputs = list(unpack([self.dut.input__TDATA.value.integer], self.n_inputs, self.input_w))
-        return inputs[idx]
-        # offset = self.input_w * idx
-        # return self.dut.input__TDATA[offset:offset+self.input_w].value.integer
-
-    def set_input_vector(self, vector):
-        for idx, value in enumerate(vector):
-            offset = self.input_w * idx
-            self.dut.input__TDATA[offset:offset+self.input_w] <= twos_comp_from_int(value, self.input_w)
-
-    def get_coeffs(self):
-        return [int_from_twos_comp(getattr(self.dut, 'coeff_'+str(i)).value.integer, self.input_w) for i in range(self.n_inputs)]
-
-    def set_coeffs(self, coeffs):
-        for i in range(len(coeffs)):
-            getattr(self.dut, 'coeff_'+str(i)) <= twos_comp_from_int(coeffs[i], self.input_w)
-
-    @cocotb.coroutine
-    def init_test(self):
-        self.dut.input__TVALID <= 0
-        self.dut.input__TLAST <= 0
-        self.dut.input__TDATA <= 0
-        self.dut.output__TREADY <= 0
-        for i in range(self.n_inputs):
-            getattr(self.dut, 'coeff_'+str(i)) <= 0
-        self.dut.rst <= 1
-        cocotb.fork(Clock(self.dut.clk, 10, 'ns').start())
-        yield RisingEdge(self.dut.clk)
-        self.dut.rst <= 0
-        yield RisingEdge(self.dut.clk)
-
-    @cocotb.coroutine
-    def input_monitor(self):
-        while True:
-            yield RisingEdge(self.dut.clk)
-            if self.dut.input__TVALID.value.integer and self.dut.input__TREADY.value.integer:
-                tmp = []
-                coeffs = self.get_coeffs()
-                for idx in range(self.n_inputs):
-                    tmp.append(int_from_twos_comp(self.get_input(idx), self.input_w))
-                self.buff_in.append(tmp)
-                self.buff_coeffs.append(coeffs)
-
-    @cocotb.coroutine
-    def output_monitor(self):
-        while True:
-            yield RisingEdge(self.dut.clk)
-            if self.dut.output__TVALID.value.integer and self.dut.output__TREADY.value.integer:
-                self.buff_out.append(int_from_twos_comp(self.dut.output__TDATA.value.integer, self.output_w))
-
-    def generate_random_vector(self):
-        limits = (-2**(self.input_w - 1), 2**(self.input_w - 1) - 1)
-        tmp = []
-        for _ in range(self.n_inputs):
-            tmp.append(random.randint(limits[0], limits[1]))
-        return tmp
-
-    def check_data(self):
-        for inputs, coeffs, o in zip(self.buff_in, self.buff_coeffs, self.buff_out):
-            assert o == sum(np.multiply(inputs, coeffs)), f'{o} != {sum(np.multiply(inputs, coeffs))} (sum(np.multiply({inputs}, {coeffs})))'
-
-    def flatten(self, vector):
-        return list(pack(vector, self.n_inputs, self.input_w))[0]
 
 @cocotb.coroutine
-def check_data(dut, burps_in, burps_out, dummy=0):
+def init_test(dut):
+    dut.rst <= 1
+    cocotb.fork(Clock(dut.clk, 10, 'ns').start())
+    yield RisingEdge(dut.clk)
+    dut.rst <= 0
+    yield RisingEdge(dut.clk)
+
+def incremental_matrix(shape, size, max_value):
+    data = []
+    count = 0
+    for i in range(size):
+        matrix = mat.create_empty_matrix(shape)
+        for idx in mat.matrix_indexes(shape):
+            mat.set_matrix_element(matrix, idx, count)
+            count = (count + 1) % max_value
+        data.append(matrix)
+    return data
+
+def check_monitors_data(input_a, input_b, output, input_w, output_w):
+    for a, b, o in zip(input_a, input_b, output):
+        parsed_a = [int_from_twos_comp(x, input_w) for x in mat.flatten(a)]
+        parsed_b = [int_from_twos_comp(x, input_w) for x in mat.flatten(b)]
+        parsed_o = int_from_twos_comp(o, output_w)
+        expected_o = sum(np.multiply(parsed_a, parsed_b))
+        assert parsed_o == expected_o, f'{parsed_o} == {expected_o}'
+
+@cocotb.coroutine
+def check_data(dut, shape, burps_in, burps_out, dummy=0):
+
     test_size = 20
+    yield init_test(dut)
 
-    test = DotProductTest(dut)
-    yield test.init_test()
-
-    m_axis = AxiStreamDriver(dut, name='input_', clock=dut.clk)
+    m_axis_a = AxiStreamMatrixDriver(dut, name='input_a_', clock=dut.clk, shape=shape)
+    m_axis_b = AxiStreamMatrixDriver(dut, name='input_b_', clock=dut.clk, shape=shape)
     s_axis = AxiStreamDriver(dut, name='output_', clock=dut.clk)
+    m_axis_a.init_sink()
+    m_axis_b.init_sink()
+    s_axis.bus.TREADY <= 0 # s_axis.init_source()
+    yield RisingEdge(dut.clk)
+
+    input_w = len(m_axis_a.get_element(m_axis_a.first_idx))
+    output_w = len(dut.output__TDATA)
     
-    coeffs = test.generate_random_vector()
-    test.set_coeffs(coeffs)
+    wr_a = [m_axis_a._get_random_data() for _ in range(test_size)]
+    wr_b = incremental_matrix(shape, test_size, 2**input_w - 1)
 
-    wr = [test.flatten(test.generate_random_vector()) for _ in range(test_size)]
-
-    cocotb.fork(test.input_monitor())
-    cocotb.fork(test.output_monitor())
-    cocotb.fork(s_axis.recv(test_size, burps_out))
-
-    yield m_axis.send(wr, burps_in)
+    cocotb.fork(m_axis_a.monitor())
+    cocotb.fork(m_axis_b.monitor())
+    cocotb.fork(s_axis.monitor())
     
-    while len(test.buff_out) < test_size:
-        yield RisingEdge(dut.clk)
+    cocotb.fork(m_axis_a.send(wr_a, burps_in))
+    cocotb.fork(m_axis_b.send(wr_b, burps=False)) # Dummy interface!
 
-    dut._log.info(f'Tested {len(test.buff_out)} cases.')
-    assert len(test.buff_out) == test_size, f'{len(test.buff_out)} == {test_size}'
-    assert len(test.buff_in) == len(test.buff_out), f'{len(test.buff_in)} == {len(test.buff_out)}'
-    test.check_data()
+    yield s_axis.recv(test_size, burps_out)
+
+    dut._log.info(f'Tested {len(s_axis.buffer)} cases.')
+    assert len(m_axis_a.buffer) == test_size, f'{len(m_axis_a.buffer)} == {test_size}'
+    assert len(m_axis_b.buffer) == test_size, f'{len(m_axis_b.buffer)} == {test_size}'
+    assert len(s_axis.buffer) == test_size, f'{len(s_axis.buffer)} == {test_size}'
+    
+    check_monitors_data(m_axis_a.buffer, m_axis_b.buffer, s_axis.buffer, input_w, output_w)
 
 
-tf_test_data = TF(check_data)
-tf_test_data.add_option('burps_in', [False, True])
-tf_test_data.add_option('burps_out', [False, True])
-tf_test_data.add_option('dummy', [0] * 5) # repeat 5 times
-tf_test_data.generate_tests()
+try:
+    string_to_tuple = lambda string: tuple([int(i) for i in string.replace('(', '').replace(')', '').split(',')])
+    running_cocotb = True
+    shape = string_to_tuple(os.environ['coco_param_shape'])
+except KeyError as e:
+    running_cocotb = False
 
-@pytest.mark.parametrize("input_w, n_inputs", [(8, 4)])
-def test_dot_product(input_w, n_inputs):
+
+if running_cocotb:
+    tf_test_data = TF(check_data)
+    tf_test_data.add_option('shape', [shape])
+    tf_test_data.add_option('burps_in', [False, True])
+    tf_test_data.add_option('burps_out', [False, True])
+    tf_test_data.add_option('dummy', [0] * 5) # repeat 5 times
+    tf_test_data.generate_tests()
+
+@pytest.mark.parametrize("input_w, shape", [(8, (4,2))])
+def test_dot_product(input_w, shape):
+    os.environ['coco_param_shape'] = str(shape)
     core = DotProduct(input_w=input_w,
-                      n_inputs=n_inputs)
+                      shape=shape)
     ports = core.get_ports()
-    run(core, 'cnn.tests.test_dot_product', ports=ports, vcd_file=f'./test_dot_product_i{input_w}_o{core.output_w}_n{n_inputs}.vcd')
+    printable_shape = '_'.join([str(i) for i in shape])
+    run(core, 'cnn.tests.test_dot_product', ports=ports, vcd_file=f'./test_dot_product_i{input_w}_shape{printable_shape}.vcd')
