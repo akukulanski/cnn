5609d8c603175deea68db245575e1aa7945263f8
akukulanski <akukulanski@gmail.com>
2020-04-15 16:40:58 -0300

    matrix port in farm

--- cnn/farm.py
+++ cnn/farm.py
@@ -1,26 +1,53 @@
 from nmigen import *
 from cnn.dot_product import DotProduct
+from cnn.interfaces import AxiStreamMatrix
 from cores_nmigen.operations import _incr
 from cores_nmigen.interfaces import AxiStream
 
 
 class Farm(Elaboratable):
-    def __init__(self, input_w, n_inputs, n_cores):
-        self.input_w = input_w
-        self.n_inputs = n_inputs
-        self.n_cores = n_cores
-        self.cores = [DotProduct(input_w, n_inputs) for _ in range(n_cores)]
-        self.output_w = self.cores[0].output_w
-        self.input = AxiStream(width=self.input_w*self.n_inputs, direction='sink', name='input')
-        self.coeff = [Signal(self.input_w, name='coeff_'+str(i)) for i in range(self.n_inputs)] # To do: unify both input interfaces.
+    #
+    # WARNING:
+    # The dataflow is controlled ONLY by the input_a AXIS interface.
+    # The input_b AXIS interface is DUMMY, and should always have valid values in the input.
+    # The ready of the input_b interface will be attached to input_a.accepted(), and a valid=1
+    # will be assumed.
+    #
+    # Why?
+    # I want to avoid a combinational path between the valid of input_b and the ready of input_a.
+    #
+    def __init__(self, input_w, shape, n_cores):
+        self.cores = [DotProduct(input_w, shape) for _ in range(n_cores)]
+        self.input_a = AxiStreamMatrix(width=input_w, shape=shape, direction='sink', name='input_a')
+        self.input_b = AxiStreamMatrix(width=input_w, shape=shape, direction='sink', name='input_b')
         self.output = AxiStream(self.output_w, direction='source', name='output')
 
     def get_ports(self):
         ports = []
-        ports += [coeff for coeff in self.coeff]
-        ports += [self.input[f] for f in self.input.fields]
+        ports += [self.input_a[f] for f in self.input_a.fields]
+        ports += [self.input_b[f] for f in self.input_b.fields]
         ports += [self.output[f] for f in self.output.fields]
         return ports
+
+    @property
+    def input_w(self):
+        return self.input_a.width
+    
+    @property
+    def n_inputs(self):
+        return self.input_a.n_elements
+
+    @property
+    def shape(self):
+        return self.input_a.shape
+
+    @property
+    def output_w(self):
+        return self.cores[0].output_w
+
+    @property
+    def n_cores(self):
+        return len(self.cores)
 
     def elaborate(self, platform):
         m = Module()
@@ -30,17 +57,24 @@ class Farm(Elaboratable):
         current_core_sink = Signal(range(self.n_cores))
         current_core_source = Signal(range(self.n_cores))
 
+        # DUMMY input_b interface
+        comb += [self.input_b.ready.eq(self.input_a.accepted())]
+
         for i, core in enumerate(self.cores):
             m.submodules['core_' + str(i)] = core
-            comb += [core.coeff[n].eq(self.coeff[n]) for n in range(self.n_inputs)] # same coefficients for everybody
+            # comb += [core.coeff[n].eq(self.coeff[n]) for n in range(self.n_inputs)] # same coefficients for everybody
+            comb += core.input_b.connect_data_ports(self.input_b) # same coefficients for everybody
             with m.If(current_core_sink == i):
-                comb += [core.input.valid.eq(self.input.valid),
-                         core.input.data.eq(self.input.data),
-                         self.input.ready.eq(core.input.ready),
+                comb += [core.input_a.valid.eq(self.input_a.valid),
+                         #core.input.data.eq(self.input.data),
+                         core.input_a.connect_data_ports(self.input_a),
+                         self.input_a.ready.eq(core.input_a.ready),
+                         self.input_b.ready.eq(core.input_b.ready),
                         ]
             with m.Else():
-                comb += [core.input.valid.eq(0),
-                         core.input.data.eq(0),
+                comb += [core.input_a.valid.eq(0),
+                         core.input_b.valid.eq(0),
+                         # core.input_a.data.eq(0),
                         ]
             with m.If(current_core_source == i):
                 comb += [self.output.valid.eq(core.output.valid),
@@ -50,7 +84,8 @@ class Farm(Elaboratable):
             with m.Else():
                 comb += [core.output.ready.eq(0),
                         ]
-        with m.If(self.input.accepted()):
+
+        with m.If(self.input_a.accepted()):
             sync += current_core_sink.eq(_incr(current_core_sink, self.n_cores))
 
         with m.If(self.output.accepted()):
