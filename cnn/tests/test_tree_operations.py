c264cb9322493e5107155461a31e1d23d4967749
akukulanski <akukulanski@gmail.com>
2020-05-03 20:50:08 -0300

    generalized pipelined tree operations with optional regs

--- cnn/tests/test_tree_operations.py
+++ cnn/tests/test_tree_operations.py
@@ -1,5 +1,5 @@
 from nmigen_cocotb import run
-from cnn.adder import TreeAdder
+from cnn.tree_operations import TreeAdder
 from cnn.tests.utils import int_from_twos_comp, subfinder, vcd_only_if_env
 import pytest
 import random
@@ -103,10 +103,10 @@ tf_test_data.add_option('dummy', [0] * 5)
 tf_test_data.generate_tests()
 
 @pytest.mark.parametrize("input_w, stages", [(8, 4), (8, 2), (8, 1),])
-def test_adder(input_w, stages):
+def test_main(input_w, stages):
     core = TreeAdder(input_w=input_w,
-                     stages=stages,
+                     n_stages=stages,
                      reg_in=True, reg_out=True)
     ports = core.get_ports()
     vcd_file = vcd_only_if_env(f'./test_adder_i{input_w}_s{stages}.vcd')
-    run(core, 'cnn.tests.test_adder', ports=ports, vcd_file=vcd_file)
+    run(core, 'cnn.tests.test_tree_operations', ports=ports, vcd_file=vcd_file)
--- cnn/tree_operations.py
+++ cnn/tree_operations.py
@@ -1,11 +1,10 @@
 from nmigen import *
 
 
-class TreeAdderStage(Elaboratable):
+class TreeStage(Elaboratable):
 
     def __init__(self, input_w, num_inputs, n, reg_in, reg_out):
-        if num_inputs % 2 != 0:
-            num_inputs += 1
+        assert num_inputs % 2 == 0
         self.inputs = [Signal(signed(input_w), name=n+'_input_'+str(i)) for i in range(num_inputs)]
         self.outputs = [Signal(signed(input_w + 1), name=n+'_output_'+str(i)) for i in range(int(num_inputs / 2))]
         self.clken = Signal()
@@ -39,29 +38,34 @@ class TreeAdderStage(Elaboratable):
         input_r = [Signal(signed(self.input_w)) for _ in self.inputs]
         sum_r = [Signal(signed(self.output_w)) for _ in self.outputs]
 
+        _operation = self._operation
+
         with m.If(self.clken):
             i_dom += [ir.eq(i) for ir, i in zip(input_r, self.inputs)]
-            sync += [s.eq(input_r[int(2*i)] + input_r[int(2*i + 1)]) for i, s in enumerate(sum_r)]
+            sync += [s.eq(_operation.__call__(input_r[int(2*i)], input_r[int(2*i + 1)])) for i, s in enumerate(sum_r)]
             o_dom += [o.eq(sr) for o, sr in zip(self.outputs, sum_r)]
 
         return m
 
 
-class TreeAdder(Elaboratable):
+class TreeOperation(Elaboratable):
+    _operation = None
 
-    def __init__(self, input_w, stages, *args, **kwargs):
-        self.stages = stages
-        self.inputs = [Signal(signed(input_w), name='input_' + str(i)) for i in range(2**(stages))]
-        self.output = Signal(signed(input_w + stages))
+    def __init__(self, input_w, n_stages, *args, **kwargs):
+        class _Stage(TreeStage):
+            _operation = self._operation
+
+        self.n_stages = n_stages
+        self.inputs = [Signal(signed(input_w), name='input_' + str(i)) for i in range(2**(n_stages))]
+        self.output = Signal(signed(input_w + n_stages))
         self.clken = Signal()
         self.args = args
         self.kwargs = kwargs
-        self.tree_adder_stages = [TreeAdderStage(self.input_w+i,
-                                            2**(self.stages-i),
-                                            n='S'+str(i),
-                                            *self.args,
-                                            **self.kwargs
-                                            ) for i in range(self.stages)]
+        self.stages = [_Stage(self.input_w+i,
+                              2**(self.n_stages-i),
+                              n='S'+str(i),
+                              *self.args,
+                              **self.kwargs) for i in range(self.n_stages)]
 
     def get_ports(self):
         ports = [self.clken] + self.inputs + [self.output]
@@ -81,28 +85,38 @@ class TreeAdder(Elaboratable):
 
     @property
     def latency(self):
-        return sum(stage.latency for stage in self.tree_adder_stages)
+        return sum(stage.latency for stage in self.stages)
 
     def elaborate(self, platform):
         m = Module()
         comb = m.d.comb
 
-        tree_adder_stages = self.tree_adder_stages
+        stages = self.stages
 
-        for i, stage in enumerate(tree_adder_stages):
-            m.submodules['tree_adder_' + str(i)] = stage
+        for i, stage in enumerate(stages):
+            m.submodules['tree_oper_' + str(i)] = stage
 
         # Clock enable to all stages
-        comb += [stage.clken.eq(self.clken) for stage in tree_adder_stages]
+        comb += [stage.clken.eq(self.clken) for stage in stages]
 
         # Connect input to first stage
-        comb += [tree_adder_stages[0].inputs[i].eq(self.inputs[i]) for i in range(self.num_inputs)]
+        comb += [stages[0].inputs[i].eq(self.inputs[i]) for i in range(self.num_inputs)]
 
-        # Pipelined adder stages
-        for prv, nxt in zip(tree_adder_stages[0:-1], tree_adder_stages[1:]):
+        # Pipelined stages
+        for prv, nxt in zip(stages[0:-1], stages[1:]):
             comb += [nxt_i.eq(prv_o) for prv_o, nxt_i in zip(prv.outputs, nxt.inputs)]
 
         # Last stage to output
-        comb += self.output.eq(tree_adder_stages[-1].outputs[0])
+        comb += self.output.eq(stages[-1].outputs[0])
 
         return m
+
+
+class TreeAdder(TreeOperation):
+    _operation = lambda self, a, b: a + b
+
+class TreeHighest(TreeOperation):
+    _operation = lambda self, a, b: Mux(a > b, a, b)
+
+class TreeLowest(TreeOperation):
+    _operation = lambda self, a, b: Mux(a < b, a, b)
