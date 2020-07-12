d0c0cdefc63242886faed605891ba59aed7842eb
akukulanski <akukulanski@gmail.com>
2020-07-11 23:31:29 -0300

    removed file

--- cnn/tests/test_matrix_interface.py
+++ cnn/tests/test_matrix_interface.py
@@ -1,5 +1,4 @@
 from nmigen_cocotb import run
-from cnn.matrix_interface import MatrixInterfaceBypass
 from cnn.tests.utils import vcd_only_if_env, incremental_matrix
 from cnn.tests.interfaces import MatrixStreamDriver
 import pytest
@@ -12,6 +11,49 @@ try:
     from cocotb.regression import TestFactory as TF
 except:
     pass
+
+from nmigen import *
+from cnn.interfaces import MatrixStream
+class MatrixInterfaceBypass(Elaboratable):
+    
+    def __init__(self, width, shape):
+        self.width = width
+        self.shape = shape
+        self.input = MatrixStream(width=width, shape=shape, direction='sink', name='input')
+        self.output = MatrixStream(width=width, shape=shape, direction='source', name='output')
+
+    def get_ports(self):
+        ports = [self.input[f] for f in self.input.fields]
+        ports += [self.output[f] for f in self.output.fields]
+        return ports
+
+    def elaborate(self, platform):
+        m = Module()
+        sync = m.d.sync
+        comb = m.d.comb
+
+        dummy = Signal() # just to force the existence of a clock domain
+        sync += dummy.eq(~dummy)
+
+        comb += [self.output.valid.eq(self.input.valid),
+                 self.output.last.eq(self.input.last),
+                 self.input.ready.eq(self.output.ready),
+                ]
+
+        comb += self.output.connect_data_ports(self.input)
+
+        #######################################################################
+        #
+        # > Alternative methods to connect dataports
+        #
+        # * Iterating through data_ports
+        #
+        # for data_i, data_o in zip(self.input.data_ports, self.output.data_ports):
+        #     comb += data_o.eq(data_i)
+        #
+        #######################################################################
+
+        return m
 
 CLK_PERIOD_BASE = 100
 
